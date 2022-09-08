1a7fec97621053b4cdcda5976172388342034fa6
David Stockdale <dstock3@gmail.com>
2022-09-08 16:10:30 -0500

    Added loading spinner to Intro component

--- src/components/basics/Intro.js
+++ src/components/basics/Intro.js
@@ -6,18 +6,20 @@ import Spinner from './Spinner';
 const Intro = ({theme}) => {
   return (
     <div className={"intro-container " + theme + "-accent"}>
+      {pawIcon ?
         <div className="logo">
-          {pawIcon ? 
-            <img src={pawIcon} alt="BlogDog logo"></img> :
-            <Spinner theme={theme} isMini={true} />}
-        </div>
+          <img src={pawIcon} alt="BlogDog logo"></img>
+        </div> :
+        <div className="logo">
+          <Spinner theme={theme} isMini={true} imgLoader={true} />
+        </div>}
         <div className="intro">Welcome to <b>BlogDog</b>, a content management system designed to maximize ease of use.</div>
-        <div className="login-prompt">
-            <Link to="/blog-dog/login">Login</Link> to access CMS features.
-        </div>
-        <div className="register-prompt">
-            Don't have an account yet? <Link to="/blog-dog/register">Register</Link> to access features.
-        </div>
+      <div className="login-prompt">
+          <Link to="/blog-dog/login">Login</Link> to access CMS features.
+      </div>
+      <div className="register-prompt">
+          Don't have an account yet? <Link to="/blog-dog/register">Register</Link> to access features.
+      </div>
     </div>
   )
 }
